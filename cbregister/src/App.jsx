060678be942fe4b916f64e7061b281049b628a70
Rohit-Singh-07 <150457028+Rohit-Singh-07@users.noreply.github.com>
2024-04-02 01:53:40 +0530

    Commit

--- cbregister/src/App.jsx
+++ cbregister/src/App.jsx
@@ -4,9 +4,16 @@ import LOGO from "./assets/LOGO.png";
 import ReactPlayer from "react-player";
 import { motion } from "framer-motion";
 import LoadingAni from "./components/LoadingAni";
+import { BiSolidVolumeMute } from "react-icons/bi";
+import { GoUnmute } from "react-icons/go";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [muted, setMuted] = useState(true);
+
+  const handleToggleMute = () => {
+    setMuted(!muted);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -31,8 +38,8 @@ const App = () => {
       ) : (
         
         <>
-          <div className="w-[100vh] absolute z-20 top-0 left-0">
-            <div className="flex justify-between items-center w-[100vw] sm:h-fit h-[70vh] mt-[25vw] sm:mt-0 sm:flex-row flex-col">
+          <div className="w-[100vh] absolute z-20 top-0 left-0 pointer-events-none">
+            <div className="flex justify-between items-center w-[100vw] sm:h-fit h-[70vh] mt-[25vw] sm:mt-0 sm:flex-row flex-col px-[3.5vw]">
               <img className="z-20 sm:w-[16vw] w-[30vw] h-[40vw] sm:h-[12vw]" src={CBLOGO} alt="" />
               <img className="z-20 sm:w-[16vw] w-[30vw] sm:h-[12vw] h-[40vw]" src={LOGO} alt="" />
             </div>
@@ -60,14 +67,16 @@ const App = () => {
               className="flex justify-center items-center h-full w-full z-10"
             >
               <ReactPlayer
-                height={800}
-                width={1550}
+                height={900}
+                width={1900}
                 playing
-                muted
-                controls
+                muted={muted}
                 loop
                 url="https://youtu.be/fp6nID4OAMA?feature=shared"
               />
+              <button className="absolute top-[50%] right-[16%] " onClick={handleToggleMute}>
+        {muted ? <span className="flex flex-col justify-center items-center"><span className="h-[6vw] w-[6vw] rounded-full z-30 bg-slate-100 bg-opacity-50 flex justify-center items-center text-[4vw] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"><BiSolidVolumeMute /></span> <h2 className="text-white text-[2vw]">Tap to Unmute</h2></span> : <span className="flex flex-col justify-center items-center"><span className="h-[6vw] w-[6vw] rounded-full z-30 bg-slate-100 bg-opacity-50 flex justify-center items-center text-[4vw] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"><GoUnmute /></span> <h2 className="text-white text-[2vw]">Tap to mute</h2></span>}
+      </button>
               <motion.div
               initial={{ scale: 1, opacity: 1 }}
               animate={{ scale: 1.5, opacity: 0 }}
@@ -89,7 +98,7 @@ const App = () => {
               >
               </motion.div>
               <a
-                className="text-[5vw] absolute md:text-[1.4vw] h-[9vw] w-[25vw] md:h-[5vw] md:w-[17vw] bg-gradient-to-r from-amber-500 to-amber-500 rounded-full flex justify-center items-center"
+                className="text-[5vw] absolute md:text-[2vw] h-[9vw] w-[25vw] md:h-[5vw] md:w-[17vw] bg-gradient-to-r from-amber-500 to-amber-500 rounded-full flex justify-center items-center"
                 href="https://codingblockslpu.vercel.app/participate"
                 target="_blank"
                 rel="noopener noreferrer"
